19537b7430958d7bdd6c9c0eb38f539b6f1f884f
Q4D1952K <97731242+Q4D1952K@users.noreply.github.com>
2024-03-12 23:17:45 +0700

    update

---
+++ ui/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import EventManager from 'utils/eventManager';
+
+import HotWire from 'pages/HotWire';
+
+
+export enum InterfaceName {
+  HotWire = 'hotWire',
+}
+
+const App = () => {
+  const [ component, setComponent ] = useState<InterfaceName | null>( InterfaceName.HotWire );
+
+  useEffect( () => {
+    EventManager.addHandler( 'router', 'setComponent', ( componentPage: InterfaceName ) => {
+      setComponent( componentPage );
+    } )
+    EventManager.stopAddingHandlers( 'router' );
+    return () => {
+      EventManager.removeTargetHandlers( 'router' );
+    };
+  }, [] )
+
+  return (
+    <>
+      {component === InterfaceName.HotWire && <HotWire />}
+    </>
+  );
+}
+
+export default App;
